139e37c7cf8ae1759684998bec707340e11a4595
Ridwan Adebosin <adebosinadewale@gmail.com>
2023-12-03 21:29:05 +0100

    Vue initialization

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <div id="app">
+    <todo-app></todo-app>
+  </div>
+</template>
+
+<script>
+import TodoApp from './components/TodoApp.vue'
+
+export default {
+  name: 'App',
+  components:{
+  TodoApp
+}
+}
+
+</script>
+
+
+<!-- 
+// const todos = ref([]);
+// const name = ref("");
+
+// const input_content = ref("");
+// const input_category = ref(null);
+
+// const todos_asc = computed(() =>
+//   todos.value.sort((a, b) => {
+//     return b.createdAt - a.createdAt;
+//   })
+// ); -->
---
+++ src/components/TodoApp.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <!-- Heading -->
+    <h2 class="heading">My vue to do app</h2>
+    <!-- Input -->
+    <div class="input">
+      <input
+        type="text"
+        v-model="task"
+        placeholder="Enter task"
+        class="form-control"
+      />
+    </div>
+  </div>
+
+  <!-- Table -->
+  <table>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+
+      <tr>
+        <th scope="row">2</th>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+
+      <tr>
+        <th scope="row">3</th>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "TodoApp",
+};
+</script>
+
+<style></style>
